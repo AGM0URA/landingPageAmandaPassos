a154631c194af903b7527bb45238ef95a62c11a3
André Gomes Moura <andregmoura06@gmail.com>
2025-03-13 09:33:29 -0300

    project base added

---
+++ Frontend/App/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+import LeadingPage from "../Pages/LeadingPage";
+
+
+const App = () => {
+  return (
+     <LeadingPage>
+     </LeadingPage>
+  );
+};
+
+export default App;
---
+++ Frontend/Pages/LeadingPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const LeadingPage = () => {
+    return(
+        <>
+        </>
+    );
+};
+
+export default LeadingPage;
