b0338519ae86e93978bc00845f56a711c5d11e50
André Gomes Moura <andregmoura06@gmail.com>
2025-03-14 08:54:54 -0300

    Filar updeted

--- Frontend/Pages/LeadingPage.jsx
+++ Frontend/Pages/LeadingPage.jsx
@@ -1,8 +1,18 @@
 import React from "react";
+import {
+    ContainerPage,
+    Logo,
+    Img01
+
+} from "../Styles/LeadingPage.js";
+import LogoImg from "../Images/logo1.png"
 
 const LeadingPage = () => {
     return(
         <>
+        <ContainerPage>
+            <Logo src={LogoImg} alt="Logo" />
+        </ContainerPage>
         </>
     );
 };
--- Frontend/Styles/LeadingPage.js
+++ Frontend/Styles/LeadingPage.js
@@ -1,8 +1,30 @@
 import styled from "styled-components";
-
+import { Color } from "./Globalstyle";
+import BackgrondIMG from "../Images/ImagemfundoTocar.png"
 
 export const ContainerPage  = styled.div`
-    
+    background-image: url(${BackgrondIMG});
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+    width: 100%px;
+    height: 1024px;
+    display: flex;
+    flex-direction: column;
 
+
+`;
+
+export const Logo = styled.img`
+    width: 200px;
+    height: auto;
+    border-radius: 10px;
+
+`;
+
+export const Img01 = styled.img`
+    width: 200px;
+    height: auto;
+    border-radius: 10px;
 
 `;
